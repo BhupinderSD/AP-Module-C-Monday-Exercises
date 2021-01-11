fed6dd75628d1adbd799d16f3d48eef7fb54ae6e
BhupinderSD <bhupinder.dhoofer@ada.ac.uk>
2021-01-11 12:37:18 +0000

    Create exercise1.cpp

---
+++ exercise1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+//https://repl.it/join/rzwfjzbr-bhupindersd
+//https://repl.it/@BhupinderSD/QuoteAQuote
+
+int main() {
+  std::string quote;
+  std::string author;
+  
+	std::cout << "What is the quote? ";
+  getline(std::cin, quote);
+
+  std::cout << "Who said it? ";
+  getline(std::cin, author);
+
+  std::cout << "\n" << author << " says: \"" << quote << "\"\n";
+  
+  return 0;
+}
